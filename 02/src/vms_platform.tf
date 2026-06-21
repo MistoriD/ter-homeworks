71c3ab84ee0711ac7b2f9d9453693d9c07abcc95
Maksim Shmagin <bac9maksim140202@bk.ru>
2026-06-21 15:52:46 +0300

    Удалил лишний символ

--- 02/src/vms_platform.tf
+++ 02/src/vms_platform.tf
@@ -30,7 +30,7 @@ variable "vms_metadata" {
     }
     db = {
       serial-port-enable  = 1
-      ssh_keys        = "ubuntu:ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4rkr7PjV2b7uISWT+a/PBlgLWSKND9Ynw6Uwnr+rbu+ivykgBuUN1Xkk8EQZYaM0y9o40mgkm4fYtFrt5+RB8mebF4wbxVrFrFE/GxEusyiu97184HdER2Cc05re8Wr2tH+skC0rnZR7DeSk8MQFjDI5pi0TU3WE6NbvxKi1s+UBf3Bo0Hv5uhU1/enz/LI+3Y1/DQZrk5iXHNbhLI2TTVrHuyAzgvQb6qKHxObhPZL7Ovt+hB3ZTHC+NFtS+gG3EPD3JYYryHdjKmBr07F7eE8nvzqk9aEDt2mHNUwF+UTHD0E3Z1G+QeixqODHsuPUK+q//XG9v/5P30ir+Egaqef/QPe/j0mdOfonTeFWG77c+5ibX/EzulzAP/wzEf3/5yDDbev6x7iRnGYpOFGd6idccVM/Q4u35jhha8lb4J+f4Lnsp+xQ6dIkB1aCYb9hv0OLyRihetHTZuriQbilqeQYb55z2sE+CKda1jU6PPbj5/a9u3D1WfgHWxvF3fh8="
+      ssh_keys        = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4rkr7PjV2b7uISWT+a/PBlgLWSKND9Ynw6Uwnr+rbu+ivykgBuUN1Xkk8EQZYaM0y9o40mgkm4fYtFrt5+RB8mebF4wbxVrFrFE/GxEusyiu97184HdER2Cc05re8Wr2tH+skC0rnZR7DeSk8MQFjDI5pi0TU3WE6NbvxKi1s+UBf3Bo0Hv5uhU1/enz/LI+3Y1/DQZrk5iXHNbhLI2TTVrHuyAzgvQb6qKHxObhPZL7Ovt+hB3ZTHC+NFtS+gG3EPD3JYYryHdjKmBr07F7eE8nvzqk9aEDt2mHNUwF+UTHD0E3Z1G+QeixqODHsuPUK+q//XG9v/5P30ir+Egaqef/QPe/j0mdOfonTeFWG77c+5ibX/EzulzAP/wzEf3/5yDDbev6x7iRnGYpOFGd6idccVM/Q4u35jhha8lb4J+f4Lnsp+xQ6dIkB1aCYb9hv0OLyRihetHTZuriQbilqeQYb55z2sE+CKda1jU6PPbj5/a9u3D1WfgHWxvF3fh8="
     }
   }
 }
